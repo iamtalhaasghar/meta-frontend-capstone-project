f9ff335110ea24f0839272be5534304ba076d576
Talha Asghar <talhaasghar220@gmail.com>
2023-03-30 21:56:25 +0500

    added products data

---
+++ src/_data.js
@@ -0,0 +1,89 @@
+export const images = [
+  {
+    id: "01",
+    src: "https://images.unsplash.com/photo-1602024242516-fbc9d4fda4b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+    alt: "Awesome watch",
+  },
+  {
+    id: "02",
+    src: "https://images.unsplash.com/photo-1451290337906-ac938fc89bce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1777&q=80",
+    alt: "Awesome watch",
+  },
+  {
+    id: "03",
+    src: "https://images.unsplash.com/photo-1568010434570-74e9ba7126bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+    alt: "Awesome watch",
+  },
+  {
+    id: "04",
+    src: "https://images.unsplash.com/photo-1569411032431-07598b0012c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    alt: "Awesome watch",
+  },
+  {
+    id: "05",
+    src: "https://images.unsplash.com/photo-1565440962783-f87efdea99fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=936&q=80",
+    alt: "Awesome watch",
+  },
+  {
+    id: "06",
+    src: "https://images.unsplash.com/photo-1548169874-53e85f753f1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80",
+    alt: "Awesome watch",
+  },
+];
+export const products = [
+  {
+    id: "1",
+    name: "Lemon Garlic Shrimp Linguine",
+    currency: "USD",
+    price: 199,
+    flag: "new",
+    imageUrl:
+      "https://images.unsplash.com/photo-1665233273469-4abcf32c5be7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8TGluZ3VpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+    rating: 4,
+    ratingCount: 100,
+    description:
+      "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    images,
+  },
+  {
+    id: "2",
+    name: "Spicy Thai Basil Chicken",
+    currency: "USD",
+    price: 199,
+    salePrice: 179.99,
+    flag: "on-sale",
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1661265874417-f9a2f1981eda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    rating: 4,
+    ratingCount: 42,
+    description:
+      "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    images,
+  },
+  {
+    id: "3",
+    name: "Classic Margherita Pizza",
+    currency: "USD",
+    price: 199,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1675451537385-e76cd7e78087?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fFBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    rating: 4,
+    ratingCount: 12,
+    description:
+      "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    images,
+  },
+  {
+    id: "4",
+    name: "Honey Mustard Glazed Salmon",
+    currency: "GBP",
+    price: 199,
+    imageUrl:
+      "https://images.unsplash.com/photo-1559058789-672da06263d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=867&q=80",
+    rating: 5,
+    ratingCount: 31,
+    description:
+      "With a sleek design and a captivating essence, this is a modern Classic made for every occasion.",
+    images,
+  },
+];
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,41 +1,113 @@
-import { Avatar, Box, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Container,
+  SimpleGrid,
+  Image,
+  Flex,
+  Heading,
+  Text,
+  Stack,
+  StackDivider,
+  Icon,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import {
+  IoAnalyticsSharp,
+  IoLogoBitcoin,
+  IoSearchSharp,
+  IoFastFoodSharp,
+} from "react-icons/io5";
+import { ReactElement } from "react";
+
+interface FeatureProps {
+  text: string;
+  iconBg: string;
+  icon?: ReactElement;
+}
+
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+  return (
+    <Stack direction={"row"} align={"center"}>
+      <Flex
+        w={8}
+        h={8}
+        align={"center"}
+        justify={"center"}
+        rounded={"full"}
+        bg={iconBg}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{text}</Text>
+    </Stack>
+  );
+};
 
 export default function AboutUs() {
   return (
-    <Stack
-      bg={useColorModeValue("gray.50", "gray.800")}
-      py={16}
-      px={8}
-      spacing={{ base: 8, md: 10 }}
-      align={"center"}
-      direction={"column"}
-    >
-      <Text>About Us</Text>
-      <Text
-        fontSize={{ base: "xl", md: "2xl" }}
-        textAlign={"center"}
-        maxW={"3xl"}
-      >
-        At our online food business, we are passionate about providing our
-        customers with delicious and high-quality meals that are prepared with
-        the freshest ingredients. Our mission is to make it easy and convenient
-        for our customers to enjoy restaurant-quality food from the comfort of
-        their homes, while also supporting local and sustainable food practices.
-      </Text>
-      <Box textAlign={"center"}>
-        <Avatar
-          src={
-            "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-          }
-          alt={"Jenny Wilson"}
-          mb={2}
-        />
-
-        <Text fontWeight={600}>Jenny Wilson</Text>
-        <Text fontSize={"sm"} color={useColorModeValue("gray.400", "gray.400")}>
-          Vice President
-        </Text>
-      </Box>
-    </Stack>
+    <Container maxW={"5xl"} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+        <Stack spacing={4}>
+          <Text
+            textTransform={"uppercase"}
+            color={"blue.400"}
+            fontWeight={600}
+            fontSize={"sm"}
+            bg={useColorModeValue("blue.50", "blue.900")}
+            p={2}
+            alignSelf={"flex-start"}
+            rounded={"md"}
+          >
+            Our Story
+          </Text>
+          <Heading>Bringing the taste of home to your doorstep.</Heading>
+          <Text color={"gray.500"} fontSize={"lg"}>
+            At our online food business, we are passionate about providing our
+            customers with delicious and high-quality meals that are prepared
+            with the freshest ingredients. Our mission is to make it easy and
+            convenient for our customers to enjoy restaurant-quality food from
+            the comfort of their homes, while also supporting local and
+            sustainable food practices.
+          </Text>
+          <Stack
+            spacing={4}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.100", "gray.700")}
+              />
+            }
+          >
+            <Feature
+              icon={
+                <Icon as={IoFastFoodSharp} color={"yellow.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("yellow.100", "yellow.900")}
+              text={"Best Taste"}
+            />
+            <Feature
+              icon={<Icon as={IoLogoBitcoin} color={"green.500"} w={5} h={5} />}
+              iconBg={useColorModeValue("green.100", "green.900")}
+              text={"Affordable Rates"}
+            />
+            <Feature
+              icon={
+                <Icon as={IoSearchSharp} color={"purple.500"} w={5} h={5} />
+              }
+              iconBg={useColorModeValue("purple.100", "purple.900")}
+              text={"Hand picked Ingredients"}
+            />
+          </Stack>
+        </Stack>
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"feature image"}
+            src={
+              "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60"
+            }
+            objectFit={"cover"}
+          />
+        </Flex>
+      </SimpleGrid>
+    </Container>
   );
 }
